30d368e8c633c8c5dd76c4c7ab654e5127d84545
harinandan <harinandan@cooltoad.com>
2012-10-08 19:36:00 +0530

    Kingdom Connectivity solution version 6

--- src/KingdomConnectivity.java
+++ src/KingdomConnectivity.java
@@ -10,94 +10,48 @@ import java.util.Stack;
 
 
 public class KingdomConnectivity {
-	static Map<Integer,BitSet> graph = new HashMap<Integer, BitSet>();
+	static int amatrix[][];
+	static int N;
+	
+
+	static int [][] apow(int a[][])
+	{
+		int ret[][] = new int[N][N];
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				for (int k = 0; k < a[i].length; k++) {
+					ret[i][j] += a[i][k] * amatrix[k][j];
+				}
+			}
+		}
+		
+		return ret;
+	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
+		N = scanner.nextInt();
+		amatrix = new int[N][N];
 		int M = scanner.nextInt();
 		for (int i = 0; i < M; i++) {
 			int S = scanner.nextInt();
 			int T = scanner.nextInt();
-			BitSet edges = graph.get(S);
-			if(edges==null)
+			amatrix[S-1][T-1] = 1;
+		}
+		//print(amatrix);
+		int[][] temp = amatrix;
+		int count = 0;
+		for (int i = 0; i < N*N; i++) {
+			temp = apow(temp);
+			if(i>= N && temp[0][N-1] > 0)
 			{
-				edges = new BitSet();
-				graph.put(S, edges);
+				System.out.println("INFINITE PATHS");
+				return;
 			}
-			edges.set(T);
+			count += temp[0][N-1]; 
+			count %= 1000000000; 
+			//print(temp);
 		}
-		
-		int val = solve(1, N, new BitSet());
-		System.out.println(val == -1? "INFINITE PATHS" : (val % 1000000000));
+		System.out.println(count);
 	}
 
-	private static int solve(int S, int N, BitSet exclude) {
-		int ret = 0;
-
-		Stack<Integer> s = new Stack<Integer>();
-		Stack<BitSet> sVisited = new Stack<BitSet>();
-		Stack<List<Integer>> sPath = new Stack<List<Integer>>();
-		s.push(S);
-		BitSet b = new BitSet();
-		b.set(S);
-		sVisited.push(b);
-		List<Integer> path = new ArrayList<Integer>();
-		path.add(1);
-		sPath.push(path);
-		while(!s.empty())
-		{
-			BitSet edges = graph.get(s.pop());
-			BitSet visited = sVisited.pop(); 
-			path = sPath.pop();
-			if(edges != null)
-			for (int i = edges.nextSetBit(0); i >= 0; i = edges.nextSetBit(i+1)) {
-				if(i == N)
-					ret++;
-				else
-				{
-					if(!visited.get(i))
-					{
-						if(!exclude.get(i))
-						{
-							s.push(i);
-							b = new BitSet();
-							b.or(visited);
-							b.set(i);
-							sVisited.push(b);
-							List<Integer> newpath = new ArrayList<Integer>();
-							newpath.addAll(path);
-							newpath.add(i);
-							sPath.push(newpath);
-							//System.out.println(newpath);
-						}
-					}
-					else
-					{
-						LinkedList<Integer> newpath = new LinkedList<Integer>();
-						newpath.addAll(path);
-						while((newpath.peek()) != i)
-							newpath.poll();
-						//System.out.println("Cycle : " + newpath);
-						if(isCycleConnected(N, newpath))
-							return -1;
-					}
-				}
-			}
-		}
-
-		return ret;
-	}
-
-	private static boolean isCycleConnected(int N,LinkedList<Integer> cycle) {
-		BitSet b = new BitSet();
-		for (Integer i : cycle) {
-			b.set(i);
-		}
-		for (Integer i : cycle) {
-			if(solve(i, N, b) != 0)
-				return true;
-		}
-		
-		return false;
-	}
 }

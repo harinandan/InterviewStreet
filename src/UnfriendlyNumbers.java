e837f168544632616ef36997daaa20d1a1a6bcdb
harinandan <harinandan@cooltoad.com>
2012-10-08 20:15:20 +0530

    Unfriendly Numbers solution version 3

--- src/UnfriendlyNumbers.java
+++ src/UnfriendlyNumbers.java
@@ -6,14 +6,15 @@ public class UnfriendlyNumbers {
 	 static BigInteger ONE = new BigInteger("1");
 	 static BigInteger ZERO = new BigInteger("0");
 	
-	static List<BigInteger> factors(BigInteger K)
+	static List<BigInteger> factors(BigInteger K, Set<BigInteger> unfriendly)
 	{
 		List<BigInteger> ret = new LinkedList<BigInteger>();
 		
 		for (BigInteger i = ONE; i.compareTo(K) <= 0; i = i.add(ONE)) {
 			if(K.remainder(i).compareTo(ZERO) == 0)
 			{
-				ret.add(i);
+				if(!unfriendly.contains(i))
+					ret.add(i);
 			}
 		}
 		
@@ -24,20 +25,21 @@ public class UnfriendlyNumbers {
 		Scanner s = new Scanner(System.in);
 		int N = s.nextInt();
 		BigInteger K = new BigInteger(s.next());
-		BigInteger unfriendly[] = new BigInteger[N];
-		for (int i = 0; i < unfriendly.length; i++) {
-			unfriendly[i] = new BigInteger(s.next());
+		Set<BigInteger> unfriendly = new HashSet<BigInteger>();
+		for (int i = 0; i < N; i++) {
+			unfriendly.add(new BigInteger(s.next()));
 		}
 		
 		int count = 0;
-		List<BigInteger> factors = factors(K);
+		List<BigInteger> factors = factors(K, unfriendly);
 		for (BigInteger factor : factors) {
-			int i;
-			for (i = 0; i < unfriendly.length; i++) {
-				if(unfriendly[i].remainder(factor).compareTo(ZERO) == 0)
+			int i = 0;
+			for (BigInteger u : unfriendly) {
+				if(u.remainder(factor).compareTo(ZERO) == 0)
 					break;
+				i++;
 			}
-			if(i == unfriendly.length)
+			if(i == unfriendly.size())
 				count++;
 		}
 		

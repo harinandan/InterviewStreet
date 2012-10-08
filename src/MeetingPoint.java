1c9db1f3329d24b0473239723e4891b71c442181
harinandan <harinandan@cooltoad.com>
2012-10-08 19:03:38 +0530

    Meeting Point solution version 6

--- src/MeetingPoint.java
+++ src/MeetingPoint.java
@@ -37,29 +37,50 @@ public class MeetingPoint
 	    }
 	}
 
-    public static List<Point> findNearestNeighbours(List<Point> points, Point p)
+    public static Point findMeetingPoint(List<Point> points)
     {
-        List<Point> neighbours = new ArrayList<Point>();
-        
-        long distance = Long.MAX_VALUE;
-        for(Point p1 : points)
-        {
-        	if(!p1.equals(p))
-        	{
-	            long p1distance = p.distance(p1);
-	            if(p1distance < distance)
-	            {
-	                distance = p1distance;
-	                neighbours.clear();
-	                neighbours.add(p1);
-	            }
-	            else if(p1distance == distance)
-	            {
-	                neighbours.add(p1);
-	            }
-        	}
-        }          
-        return neighbours;
+    	long minX;
+    	long maxX;
+    	long minY;
+    	long maxY;
+    	
+		Long[] xArr = xList.toArray(new Long[xList.size()]);
+		Long[] yArr = yList.toArray(new Long[yList.size()]);
+		
+		Arrays.sort(xArr);
+		Arrays.sort(yArr);
+    	if(xList.size()%2 == 0)
+    	{
+    		minX = xArr[xList.size()/2-1];
+    		maxX = xArr[xList.size()/2];
+    	}
+    	else
+    	{
+    		minX = maxX =  xArr[xList.size()/2];
+    	}
+    	if(yList.size()%2 == 0)
+    	{
+    		minY = yArr[yList.size()/2-1];
+    		maxY = yArr[yList.size()/2];
+    	}
+    	else
+    	{
+    		minY = maxY =  yArr[yList.size()/2];
+    	}
+    	
+    	
+    	List<Point> tPoints = new ArrayList<Point>(); 
+    	for (Point p: points) {
+			if(p.x >= minX && p.x <= maxX)
+				tPoints.add(p);
+		}
+    	
+    	
+    	for (Point p: tPoints) {
+			if(p.y >= minY && p.y <= maxY)
+				return p;
+		}
+    	return null;
     }
     
     public static long totalDistance(List<Point> points, Point center)
@@ -75,17 +96,16 @@ public class MeetingPoint
         
         return distance;
     }
+    
+    static List<Long> xList = new ArrayList<Long>();
+    static List<Long> yList = new ArrayList<Long>();
     public static void main (String args[])
     {
-      //  open up standard input
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
-      //  read the username from the command-line; need to use try/catch with the
-      //  readLine() method
       try {
           List<Point> points = new ArrayList<Point>();
-          Point mean = new Point(0, 0);
           long N = Long.parseLong(br.readLine());
           for ( int i = 0; i < N ;i++)
           {
@@ -93,37 +113,13 @@ public class MeetingPoint
               String coordinates[] = line.split(" ");
               Point p = new Point(Long.parseLong(coordinates[0]),Long.parseLong(coordinates[1]));
               points.add(p);
-              mean.x += p.x; 
-              mean.y += p.y;
+              xList.add(p.x);
+              yList.add(p.y);
           }
-          mean.x /= N;
-          mean.y /= N;
           
-          Point center = mean;
-          Point prevCenter = mean;
-          long distance = Long.MAX_VALUE;
-          while(true)
-          {
-              prevCenter = center;
-              List<Point> neighbours = findNearestNeighbours(points, center);
-              for (Point neighbour: neighbours)
-              {
-            	  if(!neighbour.v)
-            	  {
-	                  long neighbourTotalDistance = totalDistance(points, neighbour);
-	                  if(neighbourTotalDistance < distance)
-	                  {
-	                      center = neighbour;
-	                      distance = neighbourTotalDistance;
-	                  }
-	                  neighbour.visit();
-            	  }
-              }
-              if(center.equals(prevCenter))
-                  break;
-          }
 
-          System.out.println(distance);
+          Point meetingPoint = findMeetingPoint(points);
+          System.out.println(totalDistance(points, meetingPoint ));
       } catch (IOException ioe) {
          System.out.println("IO error trying to read input!");
          System.exit(1);

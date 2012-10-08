d92a5d265947641eadb3748a6be710a79dff9387
harinandan <harinandan@cooltoad.com>
2012-10-08 20:14:25 +0530

    Unfriendly Numbers solution version 2

--- src/UnfriendlyNumbers.java
+++ src/UnfriendlyNumbers.java
@@ -1,13 +1,17 @@
+import java.math.BigInteger;
 import java.util.*;
 
 
 public class UnfriendlyNumbers {
+	 static BigInteger ONE = new BigInteger("1");
+	 static BigInteger ZERO = new BigInteger("0");
 	
-	static List<Integer> factors(int K)
+	static List<BigInteger> factors(BigInteger K)
 	{
-		List<Integer> ret = new LinkedList<Integer>();
-		for (int i = 1; i <= K; i++) {
-			if((K%i) == 0)
+		List<BigInteger> ret = new LinkedList<BigInteger>();
+		
+		for (BigInteger i = ONE; i.compareTo(K) <= 0; i = i.add(ONE)) {
+			if(K.remainder(i).compareTo(ZERO) == 0)
 			{
 				ret.add(i);
 			}
@@ -19,18 +23,18 @@ public class UnfriendlyNumbers {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int N = s.nextInt();
-		int K = s.nextInt();
-		int unfriendly[] = new int[N];
+		BigInteger K = new BigInteger(s.next());
+		BigInteger unfriendly[] = new BigInteger[N];
 		for (int i = 0; i < unfriendly.length; i++) {
-			unfriendly[i] = s.nextInt();
+			unfriendly[i] = new BigInteger(s.next());
 		}
 		
 		int count = 0;
-		List<Integer> factors = factors(K);
-		for (Integer factor : factors) {
+		List<BigInteger> factors = factors(K);
+		for (BigInteger factor : factors) {
 			int i;
 			for (i = 0; i < unfriendly.length; i++) {
-				if((unfriendly[i] % factor) == 0)
+				if(unfriendly[i].remainder(factor).compareTo(ZERO) == 0)
 					break;
 			}
 			if(i == unfriendly.length)

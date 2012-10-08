3a3655088fcc3eea72920de619af16ee663461f0
harinandan <harinandan@cooltoad.com>
2012-10-08 19:44:18 +0530

    Kingdom Connectivity solution version 9

--- src/KingdomConnectivity.java
+++ src/KingdomConnectivity.java
@@ -1,113 +1,143 @@
-import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Stack;
-
-
 
 public class KingdomConnectivity {
-	static Map<Integer,BitSet> graph = new HashMap<Integer, BitSet>();
-	static Map<Integer,BitSet> connected = new HashMap<Integer, BitSet>();
-	static int N;
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		N = scanner.nextInt();
-		int M = scanner.nextInt();
-		for (int i = 0; i < M; i++) {
-			int S = scanner.nextInt();
-			int T = scanner.nextInt();
-			BitSet edges = graph.get(S);
-			if(edges==null)
-			{
-				edges = new BitSet();
-				graph.put(S, edges);
-			}
-			edges.set(T);
+	static enum Visit
+	{
+		CAN_REACH_TARGET,
+		UNVISITED
+	}
+	static class Node
+	{
+		BitSet children = new BitSet();
+		BitSet parents = new BitSet();
+		Visit visit = Visit.UNVISITED;
+		int position;
+		int paths;
+
+		int node = 0;
+
+		int getNode()
+		{
+			return node;
 		}
-		for (Map.Entry<Integer,BitSet> entry : graph.entrySet()) {
-			BitSet bs = new BitSet();
-			BitSet c = entry.getValue();
-			if(c != null)
-			{
-				bs.or(entry.getValue());
-				for (int i = bs.nextSetBit(0); i >= 0; i = bs.nextSetBit(i+1)) {
-					c = graph.get(i);
-					if(c != null)
-						bs.or(c);
-				}
+
+		void setNode(int node)
+		{
+			this.node = node;
+		}
+
+		List<Node> topologicalSort(BitSet visited)
+		{
+			visited.set(node);
+			LinkedList<Node> sortedNodes = new LinkedList<Node>();
+			for (int i = children.nextSetBit(0); i >= 0; i = children.nextSetBit(i+1)) {
+				if(!visited.get(i))
+					sortedNodes.addAll(0, graph.get(i).topologicalSort(visited));
 			}
-			connected.put(entry.getKey(), bs);
+			sortedNodes.addFirst(this);
+
+
+			return sortedNodes;
 		}
 		
-		int val = solve(1, N);
-		System.out.println(val == -1? "INFINITE PATHS" : val);
-	}
-
-	private static int solve(int S, int T) {
-		int ret = 0;
-
-		Stack<Integer> s = new Stack<Integer>();
-		Stack<BitSet> sVisited = new Stack<BitSet>();
-		Stack<LinkedList<Integer>> sPath = new Stack<LinkedList<Integer>>();
-		s.push(S);
-		BitSet b = new BitSet();
-		b.set(S);
-		sVisited.push(b);
-		LinkedList<Integer> path = new LinkedList<Integer>();
-		path.add(S);
-		sPath.push(path);
-		while(!s.empty())
+		void prune()
 		{
-			BitSet edges = graph.get(s.pop());
-			BitSet visited = sVisited.pop(); 
-			path = sPath.pop();
-			if(edges != null)
-			for (int i = edges.nextSetBit(0); i >= 0; i = edges.nextSetBit(i+1)) {
-				if(i == T)
-				{
-					ret++;
-					ret %= 1000000000;
-					for (Integer j : path) {
-						BitSet bs = null;
-						if(((bs = connected.get(T)) != null) && bs.get(j))
-							return -1;
-					}
-				}
-				else
-				{
-					if(!visited.get(i))
-					{
-						s.push(i);
-						b = new BitSet();
-						b.or(visited);
-						b.set(i);
-						sVisited.push(b);
-						LinkedList<Integer> newpath = new LinkedList<Integer>();
-						newpath.addAll(path);
-						newpath.add(i);
-						sPath.push(newpath);
-						//System.out.println(newpath);
-					}
-					else
-					{
-						while((path.peek()) != i)
-							path.poll();
-						//System.out.println("Cycle : " + newpath);
-						for (Integer j : path) {
-							BitSet bs = null;
-							if(((bs = connected.get(j)) != null) && bs.get(T))
-								return -1;
-						}
-					}
-				}
+			visit = Visit.CAN_REACH_TARGET;
+			for (int i = parents.nextSetBit(0); i >= 0; i = parents.nextSetBit(i+1)) {
+				Node parent = graph.get(i);
+				if(parent.visit == Visit.UNVISITED)
+					parent.prune();
 			}
 		}
-
-		return ret;
+		
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return "["+node+"("+visit+")"+"]";
+		}
 	}
+
+	static Map<Integer,Node> graph = new HashMap<Integer, Node>();
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int N = s.nextInt();
+		int M = s.nextInt();
+
+		for (int i = 0; i < M; i++) {
+			int S = s.nextInt();
+			int T = s.nextInt();
+			Node sNode = graph.get(S);
+			if(sNode == null)
+			{
+				sNode = new Node();
+				sNode.setNode(S);
+				graph.put(S, sNode);
+			}
+			Node tNode = graph.get(T);
+			if(tNode == null)
+			{
+				tNode = new Node();
+				tNode.setNode(T);
+				graph.put(T, tNode);
+			}
+			sNode.children.set(T);
+			tNode.parents.set(S);
+		}
+		
+		
+		//System.out.println();
+		solve(N);
+	}
+	static void solve(int N)
+	{
+		graph.get(N).prune();
+		if (!(graph.get(1).visit == Visit.CAN_REACH_TARGET))
+		{
+			System.out.println(0);
+			return;
+		}
+
+		List<Node> sorted = graph.get(1).topologicalSort(new BitSet());
+		int startPos = -1;
+		for(int i = 0; i <sorted.size();i++)
+		{
+			Node node = sorted.get(i);
+			node.position = i;
+			if(node.node == 1)
+			{
+				startPos = i;
+			}
+		}
+		Node init = sorted.get(startPos);
+		init.paths = 1;
+		for(int j = startPos+1; j <sorted.size();j++)
+		{
+			Node cur = sorted.get(j);
+			for (int i = cur.children.nextSetBit(0); i >= 0; i = cur.children.nextSetBit(i+1)) {
+				Node child = graph.get(i);
+				if(child.visit != Visit.UNVISITED && child.position <= j)
+				{
+					System.out.println("INFINITE PATHS");
+					return;
+				}
+			}
+			int paths = 0;
+			
+			for (int i = cur.parents.nextSetBit(0); i >= 0; i = cur.parents.nextSetBit(i+1)) {
+				Node prev = graph.get(i);
+				paths += prev.paths;
+			}
+			cur.paths = paths;
+		}
+		
+		System.out.println(graph.get(N).paths);
+	}
+
 }

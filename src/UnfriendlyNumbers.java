399b754c0c3a5d2c857060e5b77d9d68dd6ea1a1
harinandan <harinandan@cooltoad.com>
2012-10-08 20:15:55 +0530

    Unfriendly Numbers solution version 4

--- src/UnfriendlyNumbers.java
+++ src/UnfriendlyNumbers.java
@@ -5,18 +5,21 @@ import java.util.*;
 public class UnfriendlyNumbers {
 	 static BigInteger ONE = new BigInteger("1");
 	 static BigInteger ZERO = new BigInteger("0");
+	 static BigInteger TWO = new BigInteger("2");
 	
 	static List<BigInteger> factors(BigInteger K, Set<BigInteger> unfriendly)
 	{
 		List<BigInteger> ret = new LinkedList<BigInteger>();
+		BigInteger KBY2 = K.divide(TWO);
 		
-		for (BigInteger i = ONE; i.compareTo(K) <= 0; i = i.add(ONE)) {
+		for (BigInteger i = TWO; i.compareTo(KBY2) <= 0; i = i.add(ONE)) {
 			if(K.remainder(i).compareTo(ZERO) == 0)
 			{
 				if(!unfriendly.contains(i))
 					ret.add(i);
 			}
 		}
+		ret.add(K);
 		
 		return ret;
 	}
